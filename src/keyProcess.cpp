e3097bca6095a6e042d9a5423d58d56f2a7576af
Sanih-Nashi <interstellarcreature4@gmail.com>
2025-03-31 20:52:47 +0530

    backspace improved

--- src/keyProcess.cpp
+++ src/keyProcess.cpp
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <stdio.h>
 
 #include <utility>
 #include <iostream>
@@ -35,8 +36,9 @@ void KeyPress::ProcessKeyPress(){
     write(STDOUT_FILENO, "\r", 1);
     write(STDOUT_FILENO, lines[current_col].c_str(), lines[current_col].size());
     write(STDOUT_FILENO, "\r", 1);
-    std::string str = "\033[" + std::to_string(current_row) + "C";
-    write(STDOUT_FILENO, str.c_str(), str.size());
+    char str[7];
+    int len = snprintf(str, sizeof(str), "\033[%dC", current_row);
+    write(STDOUT_FILENO, str, len);
   }
 
   switch(c){
@@ -82,12 +84,20 @@ void KeyPress::ProcessKeyPress(){
 
         else
         {
-          write(STDOUT_FILENO, "\n\r", 2);
-	  std::string num = "\033[" + std::to_string(lines[current_col + 1].size()) + "C";
-          write(STDOUT_FILENO, num.c_str(), num.size());
-          current_row = lines[current_col + 1].size();
-        }
+	  int size = lines[current_col + 1].size();
+	  if (size != 0)
+	  {
+	    char str[9];
+	    int len = snprintf(str, sizeof(str), "\n\r\033[%dC", size); 
+	    write(STDOUT_FILENO, str, len);
+	  }
+	  else
+          {
+            write(STDOUT_FILENO, "\n\r", 2);
+	  }
 
+	  current_row = lines[current_col + 1].size();
+        }
         current_col++;
       }
       break;
@@ -106,7 +116,7 @@ void KeyPress::ProcessKeyPress(){
       if (current_row < lines[current_col].size())
       {
         write(STDOUT_FILENO, "\033[C", 3);
-	      current_row++;
+	current_row++;
       }
       break;
     }
@@ -115,23 +125,37 @@ void KeyPress::ProcessKeyPress(){
     { 
       if (current_row != 0)
       {
-        lines[current_col].erase(current_row - 1, 1);
-        write(STDOUT_FILENO, "\r", 1);
-        write(STDOUT_FILENO, "\033[K", 3);
+        lines[current_col].erase(--current_row, 1);
+        write(STDOUT_FILENO, "\r\033[K", 4);
         write(STDOUT_FILENO, lines[current_col].c_str(), lines[current_col].size());
-        write(STDOUT_FILENO, "\r", 1);
-        std::string row = "\033[" + std::to_string(--current_row) + "C";
-        write(STDOUT_FILENO, row.c_str(), row.size());
+	write(STDOUT_FILENO, "\r", 1);
+	if (current_row != 0){
+          std::string row = "\033[" + std::to_string(current_row) + "C";
+          write(STDOUT_FILENO, row.c_str(), row.size());
+        } 	
       }
       else
       {  
         if (current_col != 0)
 	{
-	  write(STDOUT_FILENO, "\033[A", 3);
+
 	  std::string num = "\033[" + std::to_string(lines[current_col - 1].size()) + "C";
-          write(STDOUT_FILENO, num.c_str(), num.size());
-          current_row = lines[current_col - 1].size();
-	  current_col--;
+	  lines[current_col - 1] = lines[current_col - 1] + lines[current_col];
+	  lines.erase(lines.begin() + current_col--);
+          current_row = lines[current_col].size() - 1;
+
+	  write(STDOUT_FILENO, "\033[A", 3);
+	  write(STDOUT_FILENO, lines[current_col].c_str(), lines[current_col].size());
+
+	  for (int i = current_col + 1; i < lines.size(); i++){
+	    write(STDOUT_FILENO, "\n\r\033[K", 5);
+	    write(STDOUT_FILENO, lines[i].c_str(), lines[i].size());
+	  }        
+	    std::string num2 = "\033[" + std::to_string(lines.size() - current_col) + "A";
+	    write(STDOUT_FILENO, "\n\033\r[K", 5);
+	    write(STDOUT_FILENO, num2.c_str(), num2.size());
+	    write(STDOUT_FILENO, num.c_str(), num.size());
+
 	}
       }  
       break;
